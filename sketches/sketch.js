44d31f68c9b6c1f464dee6cd1435d0cff12ace04
hhae342 <209975781+hhae342@users.noreply.github.com>
2025-06-19 23:44:21 +0900

    fix: refine particle color and scaling in sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -15,7 +15,7 @@ function setup() {
         y: idY,
         vx: 0,
         vy: 0,
-        colour: color(random(170, 255), 100, 100),
+        colour: color(random(170, 255), 100, 95),
       });
     }
   }
@@ -68,7 +68,7 @@ function draw() {
     }
 
     fill(particle.colour);
-    let scale = random(1, 3);
+    let scale = random(2, 3);
     ellipse(particle.x, particle.y, scale);
   }
 }
