86f556fe023f6afa5732a4ccc3e691ad38033d0a
hhae342 <209975781+hhae342@users.noreply.github.com>
2025-06-19 23:57:26 +0900

    fix: refine noise parameters for particle movement in sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -31,14 +31,18 @@ function draw() {
     let particle = particles[i];
 
     let Vx = map(
-      noise(particle.x * 0.00001, particle.y * 0.005, frameCount * 0.001),
+      noise(particle.x * 0.00001, particle.y * 0.0001, frameCount * 0.001),
       0,
       1,
       minSpeed,
       maxSpeed
     );
     let Vy = map(
-      noise(particle.y * 0.00001, particle.x * 0.005, frameCount * 0.01 + 1000),
+      noise(
+        particle.y * 0.00001,
+        particle.x * 0.0001,
+        frameCount * 0.01 + 1000
+      ),
       0,
       1,
       minSpeed,
