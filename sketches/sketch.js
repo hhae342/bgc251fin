2cf7bb99a94d9d48c0ba836acf44412efd127096
hhae342 <209975781+hhae342@users.noreply.github.com>
2025-06-19 23:11:00 +0900

    fix: improve particle movement dynamics in sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -4,18 +4,18 @@ let particles = [];
 const r = 3;
 
 function setup() {
-  new Responsive().createResponsiveCanvas(800, 600, 'contain', true);
+  new Responsive().createResponsiveCanvas(800, 400, 'contain', true);
   background(0);
   colorMode(HSB);
 
-  for (let idY = 0; idY < height; idY += 20) {
-    for (let idX = 0; idX < width; idX += 20) {
+  for (let idY = 0; idY < height; idY += 15) {
+    for (let idX = 0; idX < width; idX += 15) {
       particles.push({
         x: idX,
         y: idY,
         vx: 0,
         vy: 0,
-        colour: color(random(190, 255), 100, 100),
+        colour: color(random(170, 255), 100, 100),
       });
     }
   }
@@ -24,21 +24,20 @@ function setup() {
 function draw() {
   colorMode(HSB);
   background('black');
-  let minSpeed = map(mouseX, 0, width, 1, 2);
-  let maxSpeed = map(mouseY, 0, height, 3, 6);
-
+  let minSpeed = map(mouseX, 0, width, 1, 3);
+  let maxSpeed = map(mouseY, 0, height, 4, 7);
   for (let i = 0; i < particles.length; i++) {
     let particle = particles[i];
 
     let Vx = map(
-      noise(particle.x * 0.00001, particle.y * 0.0008, frameCount * 0.01),
+      noise(particle.x * 0.00001, particle.y * 0.005, frameCount * 0.001),
       0,
       1,
       minSpeed,
       maxSpeed
     );
     let Vy = map(
-      noise(particle.y * 0.00001, particle.x * 0.0008, frameCount * 0.01 + 800),
+      noise(particle.y * 0.00001, particle.x * 0.005, frameCount * 0.01 + 1000),
       0,
       1,
       minSpeed,
@@ -48,28 +47,28 @@ function draw() {
     particle.vx += Vx - (minSpeed + maxSpeed) / 2;
     particle.vy += Vy - (minSpeed + maxSpeed) / 2;
 
-    particle.x += particle.vx * 0.02;
-    particle.y += particle.vy * 0.02;
+    particle.x += particle.vx * 0.01;
+    particle.y += particle.vy * 0.01;
 
     if (particle.x < r) {
       particle.x = r;
-      particle.vx *= -1.0;
+      particle.vx *= -0.6;
     }
     if (particle.x > width - r) {
       particle.x = width - r;
-      particle.vx *= -1.0;
+      particle.vx *= -0.6;
     }
     if (particle.y < r) {
       particle.y = r;
-      particle.vy *= -1.0;
+      particle.vy *= -0.6;
     }
     if (particle.y > height - r) {
       particle.y = height - r;
-      particle.vy *= -1.0;
+      particle.vy *= -0.6;
     }
 
     fill(particle.colour);
-    let scale = random(1, 4);
+    let scale = random(1, 3);
     ellipse(particle.x, particle.y, scale);
   }
 }
