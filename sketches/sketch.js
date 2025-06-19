cea6b78f5b166ff3f47523b8d4f08caefe4b2749
hhae342 <209975781+hhae342@users.noreply.github.com>
2025-06-19 23:50:08 +0900

    fix: update particle behavior and rendering in sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -24,6 +24,7 @@ function setup() {
 function draw() {
   colorMode(HSB);
   background('black');
+  noStroke();
   let minSpeed = map(mouseX, 0, width, 1, 3);
   let maxSpeed = map(mouseY, 0, height, 4, 7);
   for (let i = 0; i < particles.length; i++) {
