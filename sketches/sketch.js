9c80e8a3383ea969a62b6216d82b74143c69eeab
hhae342 <209975781+hhae342@users.noreply.github.com>
2025-06-20 00:23:30 +0900

    fix: refine speed mapping and noise parameters for particle movement in sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -25,13 +25,13 @@ function draw() {
   colorMode(HSB);
   background('black');
   noStroke();
-  let minSpeed = map(mouseX, 0, width, 1, 3);
-  let maxSpeed = map(mouseY, 0, height, 4, 6);
+  let minSpeed = map(mouseX, 0, width, 1, 2);
+  let maxSpeed = map(mouseY, 0, height, 3, 4);
   for (let i = 0; i < particles.length; i++) {
     let particle = particles[i];
 
     let Vx = map(
-      noise(particle.x * 0.00001, particle.y * 0.0095, frameCount * 0.001),
+      noise(particle.x * 0.00001, particle.y * 0.009, frameCount * 0.001),
       0,
       1,
       minSpeed,
@@ -40,8 +40,8 @@ function draw() {
     let Vy = map(
       noise(
         particle.y * 0.00001,
-        particle.x * 0.0095,
-        frameCount * 0.01 + 1000
+        particle.x * 0.009,
+        frameCount * 0.001 + 1000
       ),
       0,
       1,
